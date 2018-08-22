be25055466533f147373c18e82eb3a823d12fb1f
higuoxing@gmail.com <higuoxing@outlook.com>
2018-08-22 14:06:18 +0800

    init

--- pat/1100.cc
+++ pat/1100.cc
@@ -4,52 +4,54 @@ using namespace std;
 int main() {
   int n;
   string lut[] = {
-    "tret" , "jan"  , "feb" , 
-    "mar"  , "apr"  , "may" , 
-    "jun"  , "july" , "aug" , 
-    "sep"  , "oct"  , "nov" , 
-    "dec"  , "tam"  , "hel" , 
-    "maa"  , "huh"  , "tou" , 
-    "kes"  , "hei"  , "elo" , 
-    "syy"  , "lok"  , "mer" , 
-    "jou"  , 
+    "tret" , "jan"  , "feb" ,
+    "mar"  , "apr"  , "may" ,
+    "jun"  , "july" , "aug" ,
+    "sep"  , "oct"  , "nov" ,
+    "dec"  , "tam"  , "hel" ,
+    "maa"  , "huh"  , "tou" ,
+    "kes"  , "hei"  , "elo" ,
+    "syy"  , "lok"  , "mer" ,
+    "jou"
   };
   scanf("%d\n", &n);
   for (int i = 0; i < n; ++ i) {
     string num;
     getline(cin, num);
     if (isdigit(num[0])) {
-      // is on earth
-      int earth_num = stoi(num), m = 0, n = 0;
-      m = earth_num / 13;
-      n = earth_num % 13;
-      if (m != 0)
-        printf("%s ", lut[12+m].c_str());
+      // this is 10-digit
+      int n = stoi(num) / 13,
+          m = stoi(num) % 13;
       if (n != 0)
-        printf("%s\n", lut[n].c_str());
+        cout << lut[n+12];
+      if (n != 0 && m != 0)
+        cout << ' ' << lut[m];
+      else if (n == 0)
+        cout << lut[m];
     } else {
+      // 13-digits
       string m = "", n = "";
-      int f = 0, i_m = 0, i_n = 0;
+      int f = 0;
       for (int j = 0; j < num.length(); ++ j) {
+        if (num[j] == ' ')
+          f = 1;
         if (f == 0 && num[j] != ' ')
           m.push_back(num[j]);
-        else if (num[j] == ' ')
-          f = 1;
-        else
+        if (f == 1 && num[j] != ' ')
           n.push_back(num[j]);
       }
-      for (int j = 0; j < 25; ++ j) {
-        if (lut[j] == m)
-          i_m = j;
-        if (lut[j] == n)
-          i_n = j;
+      int i_m = 0, i_n = 0;
+      for (int k = 0; k < 25; ++ k) {
+        if (lut[k] == m)
+          i_m = k;
+        if (lut[k] == n)
+          i_n = k;
       }
-      if (i_m >= 13)
-        i_m = 13 * (i_m - 12);
-      if (i_n >= 13)
-        i_n = 13 * (i_n - 12);
-      printf("%d\n", i_m + i_n);
+      if (i_m > 12)
+        i_m = 13*(i_m - 12);
+      cout << i_m + i_n;
     }
+    cout << endl;
   }
   return 0;
 }
