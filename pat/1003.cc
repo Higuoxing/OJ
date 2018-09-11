82d305b05f2ed26db131985385817856c446f11e
higuoxing@gmail.com <higuoxing@outlook.com>
2018-09-11 12:50:11 +0800

    init

--- pat/1003.cc
+++ pat/1003.cc
@@ -1,71 +1,47 @@
-#include <iostream>
 #include <algorithm>
+#include <iostream>
+#include <vector>
 using namespace std;
+const int inf = 999999;
+int m, n, s, e,
+    teams[501], dis[501], vw[501] = { 0 },
+    graph[501][501], nums[501];
+bool visited[501] = { false };
 int main() {
-  int n, m, s, e;
-  const int inf = 99999999;
-  int vw[510], dis[510], num[510], w[510];
-  int g[510][510];
-  bool visited[510];
-
-  // init vars
-  fill(visited, visited+510, false);
-  fill(num, num+510, 0);
-  fill(vw, (vw+510), 0);
-  fill(w, (w+510), 0);
-  fill(g[0], (g[0]+510*510), inf);
-  fill(dis, (dis+510), inf);
-
-  scanf("%d %d %d %d", &n, &m, &s, &e);
-
+  scanf("%d %d %d %d", &m, &n, &s, &e);
+  fill(graph[0], graph[0]+501*501, inf);
+  fill(dis, dis+501, inf);
+  for (int i = 0; i < m; ++ i) scanf("%d", &teams[i]);
   for (int i = 0; i < n; ++ i) {
-    scanf("%d", &vw[i]);
-    g[i][i] = 0;
+    int s0, s1, d;
+    scanf("%d %d %d", &s0, &s1, &d);
+    graph[s0][s1] = graph[s1][s0] = d;
   }
+  nums [s] = 1;
+  dis  [s] = 0;
+  vw   [s] = teams[s];
   for (int i = 0; i < m; ++ i) {
-    int v1, v2, d;
-    scanf("%d %d %d", &v1, &v2, &d);
-    g[v1][v2] = g[v2][v1] = d;
-  }
-
-  dis[s] = 0;
-  w[s] = vw[s];
-  num[s] = 1;
-  // traverse all nodes
-  for (int i = 0; i < n; ++ i) {
-
-    int minn = inf;
-    int v = -1;
-
-    // traverse neighbors
-    for (int j = 0; j < n; ++ j) {
+    int minn = inf, v = -1;
+    for (int j = 0; j < m; ++ j) {
       if (!visited[j] && dis[j] < minn) {
-        v = j;
         minn = dis[j];
+        v = j;
       }
     }
-
-    if (v == -1)
-      break;
-
+    if (v == -1) break;
     visited[v] = true;
-
-    // update path
-    for (int j = 0; j < n; ++ j) {
-      if (!visited[j] && minn + g[v][j] < dis[j]) {
-        dis[j] = minn + g[v][j];
-        w[j] = w[v] + vw[j];
-        num[j] = num[v];
-      } else if (!visited[j] && minn + g[v][j] == dis[j]) {
-        dis[j] = minn + g[v][j];
-        num[j] += num[v];
-        if (w[j] < w[v] + vw[j])
-          w[j] = w[v] + vw[j];
+    for (int j = 0; j < m; ++ j) {
+      if (!visited[j] && graph[v][j] + minn < dis[j]) {
+        dis[j] = minn + graph[v][j];
+        vw[j] = vw[v] + teams[j];
+        nums[j] = nums[v];
+      } else if (!visited[j] && graph[v][j] + minn == dis[j]) {
+        nums[j] += nums[v];
+        if (vw[j] < vw[v] + teams[j])
+          vw[j] = vw[v] + teams[j];
       }
     }
   }
-
-  printf("%d %d", num[e], w[e]);
-
+  printf("%d %d", nums[e], vw[e]);
   return 0;
 }
